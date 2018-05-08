d6b2da422777803f7ef1bb4257bf2c4b72419178
Alptekin <alptekin.onder@ug.bilkent.edu.tr>
2018-05-08 11:43:36 +0300

    Test

--- app/src/main/java/com/example/alptekin/bilconnectt/announcementsFragment.java
+++ app/src/main/java/com/example/alptekin/bilconnectt/announcementsFragment.java
@@ -6,26 +6,58 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 /**
  * Created by Alptekin on 7.05.2018.
  */
 
 public class announcementsFragment extends Fragment {
-    @Nullable
-    private RecyclerView recyclerView;
+
+    private RecyclerView announcementsRecyclerView;
+    private FirebaseFirestore firebaseFirestore;
+    private String TAG = "FireLog";
+    private List<Announcement> announcementsList;
     private AnnouncementsListAdapter announcementsListAdapter;
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_announcement_fragment,container,false);
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_announcements);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(announcementsListAdapter);
+        announcementsRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_announcements);
+        announcementsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        announcementsRecyclerView.setAdapter(announcementsListAdapter);
+        firebaseFirestore.collection("Announcements").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                //if there is any error
+                if( e != null)
+                {
+                    Log.d(TAG, e.getMessage());
+                }
+                for(DocumentChange doc: documentSnapshots.getDocumentChanges())
+                {
+                    Announcement announcement = doc.getDocument().toObject(Announcement.class);
+                    Log.d(TAG, announcement.getTopic());
+                    announcementsList.add(announcement);
+                }
+
+            }
+        });
+
         return view;
+        //retrieving data from collection
+
     }
 }
